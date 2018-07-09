1df80e82725887fc6e82928759e7247f31ac3cb9
deansher <deansherthompson@gmail.com>
2018-07-09 17:32:54 -0400

    Tidied up namespace names.

--- src/joy_prime/core.clj
+++
@@ -1,6 +0,0 @@
-(ns joy-prime.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/joy_prime/types_test.clj
+++ test/joy_prime/types_test.clj
@@ -1,6 +1,6 @@
-(ns joy-prime.core-test
+(ns joy-prime.types-test
   (:require [clojure.test :refer :all]
-            [joy-prime.core :refer :all]))
+            [joy-prime.types :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
